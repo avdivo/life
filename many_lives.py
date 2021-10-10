4cd038efd5acf8a0581bc90b70d2de6803e9269e
avdivo <avdivo@mail.ru>
2021-10-10 21:44:07 +0300

    Версия PyGame с 6 цветами. Закончена

--- many_lives.py
+++ many_lives.py
@@ -1,5 +1,6 @@
 import pygame
-import sys, time, queue
+import sys, time, queue, random
+from collections import Counter
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
@@ -22,7 +23,8 @@ class Rectangle(object):
     def __init__(self, sc, x, y):
         self.x = x  # Горизонтальная координата сетки
         self.y = y  # Вертикальная координата сетки
-        self.color = 9  # Цвет клетки (9 - нет цвета, остальные цвета как в Palette)
+        self.color = 6  # Цвет клетки (6 - нет цвета, остальные цвета как в Palette)
+        self.new_color = 6
 
         global size_points
         coords = (x * (size_points + 1) + 1, y * (size_points + 1) + 1,
@@ -31,16 +33,23 @@ class Rectangle(object):
         self.rect = pygame.draw.rect(sc, self.color_1, coords)
 
     # Переключение клеточки
-    def change_color(self, color):
+    def change_color(self, color_num=-1):
         global counter_points
-        if self.color == palette.activ_rect.color:
-            color = self.color_1
-            counter_points -= 1
+        if color_num == -1:
+            color_num = palette.activ_rect.color_num
+            if self.color == color_num:
+                color = 6
+                counter_points -= 1
+            else:
+                color = palette.activ_rect.color_num
+                counter_points += 1 if self.color == 6 else 0
         else:
-            color = palette.activ_rect.color
-            counter_points += 1 if self.color == 9 else 0
-            pygame.draw.rect(self.sc, color, self.rect)
-        count_point.update_label(str(counter_points))
+            color = color_num
+            counter_points += 1 if color != 6 else -1
+        pygame.draw.rect(self.sc, palette.colors[color], self.rect)
+        count_point.update_label(str(counter_points)) # Меняем общий счетчик клеточек
+        palette.change_color(self.color, color)
+        self.color = color
 
 
 
@@ -141,47 +150,56 @@ class Label():
 
 class Palette():
     # Палитра для выбора активного цвета и отображения количества клеточек каждого цвета
-    colors = ((0, 0, 0), (255, 255, 255), (255, 255, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255))
-    activ_rect = None  # Объект клетки выбора активного цвета
 
     def __init__(self, sc, x, y):
         # Цвета: 0 - черный, 1 - белый, 2 - желтый, 3 - красный, 4 - зеленый, 5 - синий
-        # self.counts_colors = (0, 0, 0, 0, 0, 0)
+        self.colors = ((0, 0, 0), (255, 255, 255), (255, 255, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (100, 100, 100))
         self.x = x # Координаты палитры по горизонтали
         self.y = y # Координаты палитры по вертикали
         self.width = 45 # Ширина клеточки с рамкой
         self.height = 30 # Высота клеточки с рамкой
         self.gap = 4 # Расстояние между клеточками
+        self.cells = [] # Объекты клеток
         r = pygame.draw.rect(sc, (255, 255, 255), (self.x, self.y, self.width*2+self.gap, self.height), 1)
-        Palette.activ_rect = self.Cell(sc, r, self.colors[3]) # Объект с rect и цветом
-        Palette.activ_rect.draw_cell()
+        self.activ_rect = self.Cell(self, sc, r, 3) # Объект с rect и цветом
+        self.activ_rect.draw_cell()
 
         for i in range(6):
             y = self.y + (self.height + self.gap) * ((i+2)//2)
             x = self.x + (self.width + self.gap) * (i%2)
             r = pygame.draw.rect(sc, (255, 255, 255), (x, y, self.width, self.height), 1) # Объект Rect клетки цвета
-            new_cell = self.Cell(sc, r, self.colors[i])
-            self.Cell.rects.append(new_cell) # Объект с rect и цветом
+            new_cell = self.Cell(self, sc, r, i)
+            self.cells.append(new_cell) # Объект с rect и цветом
             new_cell.draw_cell()
             new_cell.write_cell()
 
+    def change_color(self, color_old, color_new):
+        # Учет счетчиков цветов клеточек в палитре. Получает старый и новый цвета
+        if color_old < 6:
+            cell = self.cells[color_old]
+            cell.color_counter_plus(-1)
+        if color_new < 6:
+            cell = self.cells[color_new]
+            cell.color_counter_plus(1)
 
-    def isPress(pos):
+    def isPress(self, pos):
         # Проверка, нажата ли одна из клеток палитры
-        for cell in Palette.Cell.rects:
+        for cell in self.cells:
             if cell.rect.collidepoint(pos):
-                Palette.activ_rect.color = cell.color
-                Palette.activ_rect.draw_cell()
+                self.activ_rect.color = cell.color
+                self.activ_rect.color_num = cell.color_num
+                self.activ_rect.draw_cell()
 
     class Cell():
         # Класс хранит цветовые ячейки и объекты Rect
-        rects = []
 
-        def __init__(self, sc, rect, color):
+        def __init__(self, self_up, sc, rect, color_num):
             self.rect = rect
-            self.color = color
+            self.color = self_up.colors[color_num]
+            self.color_num = color_num
             self.sc = sc
             self.count = 0 # Количество клеточек этого цвета
+            self.self_up = self_up # Объект выше (этот класс вложенный)
 
         def draw_cell(self):
             # Принимает объект rect, закрашивает внутреннюю часть с отступом
@@ -196,6 +214,14 @@ class Palette():
             self.sc.blit(text,
                          (self.rect.x + (self.rect.width / 2 - text.get_width() / 2),
                           self.rect.y + (self.rect.height / 2 - text.get_height() / 2)))
+
+        def color_counter_plus(self, param):
+            # Увеличиваем или уменьшаем счетчик цвета, ячейку перерисовываем
+            # Принимает 1 или -1
+            self.count += param
+            self.draw_cell()
+            self.write_cell()
+
 
 #            Функции
 
@@ -225,7 +251,8 @@ def step_fun():
 def click_clear():
     for y in range(size_y):
         for x in range(size_x):
-            if field[y][x].activ:
+            if field[y][x].color < 6:
+                field[y][x].new_color = 6
                 change.put(field[y][x])
     out()
     count_reset_fun()
@@ -239,17 +266,30 @@ def open_file():
     root.destroy()
     if name_file:
         with open(name_file, encoding='utf-8') as f:
-            new = f.read().split(' ')
+            new = f.read().split('\n')
+            color = []
+            if new[0] == '-3-':
+                color = new[2]
+                del new[2]
+                del new[0]
+            new = new[0].split(' ')
             del new[-1]
+            if not len(color):
+                color = '3' * (len(new) // 2)
+            color = list(color)
             click_clear()
-            for i in range(0, len(new), 2):
-                change.put(field[int(new[i + 1])][int(new[i])])
+            for i in range(len(color)):
+                field[int(new[i*2 + 1])][int(new[i*2])].new_color = int(color[i])
+                change.put(field[int(new[i*2 + 1])][int(new[i*2])])
         out()
         count_reset_fun()
 
 
 # Кнопка Сохранить.
-# Сохраняет координаты x, y через пробелы активных клеточек
+# Сохраняет координаты x, y через пробелы только активных клеточек
+# Второй строкой цвета этих клеточек через пробелы
+# В первой строке -3- Для этой версии программы
+
 def save_file():
     root = Tk()
     root.withdraw()
@@ -259,10 +299,13 @@ def save_file():
         if name_file[len(name_file) - 5:] != '.life':
             name_file += '.life.'
         st = ''
+        color = ''
         for y in range(size_y):
             for x in range(size_x):
-                if field[y][x].activ:
+                if field[y][x].color < 6:
                     st += str(field[y][x].x) + ' ' + str(field[y][x].y) + ' '
+                    color += str(field[y][x].color)
+        st = '-3-\n' + st + '\n' + color
         with open(name_file, "w") as f:
             f.write(st)
 
@@ -309,7 +352,8 @@ def out():
     global counter_step, step
     auto_stop = True
     while not change.empty():
-        change.get().change_color()
+        cell = change.get()
+        cell.change_color(cell.new_color)
         auto_stop = False
     if auto_stop or step:
         if run:
@@ -360,7 +404,7 @@ speed_plus_btn = Button(sc, w - 40, 320, 20, 25, '>', speed_plus)
 
 palette = Palette(sc, w - 112, 400)
 
-exit_btn = Button(sc, w - 110, 600, 70, 25, 'Выход', the_end)
+exit_btn = Button(sc, w - 110, 732, 70, 25, 'Выход', the_end)
 
 
 # обновляем окно
@@ -369,25 +413,41 @@ pygame.display.update()
 while 1:
     stat_time = time.time()
     if run:
+        t1 = time.time()
         for y in range(size_y):
             for x in range(size_x):
-                neighbors = field[y][x - 1].activ
-                neighbors += field[y - 1][x - 1].activ
-                neighbors += field[y - 1][x].activ
-                neighbors += field[y - 1][(x + 1) % size_x].activ
-                neighbors += field[y][(x + 1) % size_x].activ
-                neighbors += field[(y + 1) % size_y][(x + 1) % size_x].activ
-                neighbors += field[(y + 1) % size_y][x].activ
-                neighbors += field[(y + 1) % size_y][x - 1].activ
+                neighbors = []
+                neighbors.append(field[y][x - 1].color)
+                neighbors.append(field[y - 1][x - 1].color)
+                neighbors.append(field[y - 1][x].color)
+                neighbors.append(field[y - 1][(x + 1) % size_x].color)
+                neighbors.append(field[y][(x + 1) % size_x].color)
+                neighbors.append(field[(y + 1) % size_y][(x + 1) % size_x].color)
+                neighbors.append(field[(y + 1) % size_y][x].color)
+                neighbors.append(field[(y + 1) % size_y][x - 1].color)
 
-                life = False
-                if neighbors == 2 and field[y][x].activ:
-                    life = True
-                if neighbors == 3:
-                    life = True
-                if field[y][x].activ != life:
-                    # Клеточку необходимо переключить
+                neighbors = Counter(neighbors)
+                del neighbors[6] # Удаляем количество пустых клеток вокруг исследуемой
+                count_neighbors = sum(neighbors.values()) # Клеточек вокруг заполнено
+
+                if count_neighbors == 3 and field[y][x].color == 6:
+                    # Появление новой клеточки
+                    if max(neighbors.values()) == 1:
+                        # Клеточки все разных цветов, родитель случайный из них
+                        field[y][x].new_color = random.choice(tuple(neighbors.keys()))
+                    else:
+                        # 2 или 3 клеточки одинаковые, родитель по большинству
+                        field[y][x].new_color = max(neighbors, key=neighbors.get)
                     change.put(field[y][x])  # Помещаем объект в очередь на переключение
+                else:
+                    if field[y][x].color < 6 and (count_neighbors < 2 or count_neighbors > 3):
+                        # Удаление клеточки
+                        field[y][x].new_color = 6
+                        change.put(field[y][x])  # Помещаем объект в очередь на переключение
+
+        print(time.time() - t1)
+
+
 
         out()
 
@@ -410,7 +470,7 @@ while 1:
                     # По клеткам не щелкали, проверяем не по кнопкам ли
                     elif not Button.isPress(event.pos):
                         # Не по кнопкам, проверяем палитру
-                        Palette.isPress(event.pos)
+                        palette.isPress(event.pos)
 
 
             elif event.type == userEvent:
